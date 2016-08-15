aac594db8f0a5df7eeac4e2878549e839d59a077
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-16 01:54:21 +0300

    Run scripts faster, on DOMContentLoaded, not on load

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.addEventListener("DOMContentLoaded", function () {
   var textarea = document.querySelector('#code textarea');
   var editor = CodeMirror.fromTextArea(textarea, {
     lineNumbers: true
